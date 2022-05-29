dec73b067c7138e2adc4db7d9305faf4a3443107
Abhishek Yadav <abhishekaug2002@gmail.com>
2022-05-29 22:42:02 +0530

    Sponsor logo added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Nav.css';
 import Logo from './IEEE Logo.png'
+import Logo1 from './Salesforce Logo.png'
 
 export default function Navbar() {
   return (
@@ -11,6 +12,10 @@ export default function Navbar() {
       <span className="logo">
        <a href="https://www.ieeesrmist.in/">
       <img src={Logo} height="50" width="120" style={{"margin-left":"20px"}} alt="IEEE LOGO" /></a>
+      </span>
+      <span className="logo1">
+       <a href="/">
+      <img src={Logo1} height="60" width="60" style={{"margin-left":"10px"}} alt="SALESFORCE LOGO" /></a>
       </span>
         </div>
         <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
