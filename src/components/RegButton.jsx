9f3f61f37bfcef4b5389bcc9032fd153ccbc4dc6
some-earth11 <sk8762@srmist.edu.in>
2022-05-29 22:34:01 +0530

    added CTA to Reg-Button

--- src/components/RegButton.jsx
+++ src/components/RegButton.jsx
@@ -1,6 +1,6 @@
 const RegButton = () => {
     return ( 
-        <div data-tf-popover="Crhhk5pS" data-tf-custom-icon="https://images.typeform.com/images/TztgEuKUG3UM" data-tf-button-color="#0445AF" data-tf-button-text="Launch me" data-tf-iframe-props="title=IEEE Bootcamp Testing " data-tf-medium="snippet" style={{all: 'unset'}} />
+        <div data-tf-popover="Crhhk5pS" data-tf-tooltip="Click here to Register" data-tf-custom-icon="https://images.typeform.com/images/TztgEuKUG3UM" data-tf-button-color="#0445AF" data-tf-button-text="Launch me" data-tf-iframe-props="title=IEEE Bootcamp Testing " data-tf-medium="snippet" style={{all: 'unset'}}  />
      );
 }
  
