10e254c34214d0df41969a9edf4cdc2c90612ef2
Kunal Keshan <kunalkeshan12@gmail.com>
2022-06-03 17:19:42 +0530

    added fontawesome and about webture structure

--- src/components/AboutWebture/AboutWebture.jsx
+++ src/components/AboutWebture/AboutWebture.jsx
@@ -7,8 +7,12 @@ import React from 'react';
 
 function AboutWebture() {
   return (
-    <div>
-      
+    <div className='container'>
+      <div className='container__browser'>
+        <div className='browser__tab'>Awesome Web Browser</div>
+        <div className='browser__navigation'></div>
+        <div className='browser__body'></div>
+      </div>
     </div>
   )
 }
