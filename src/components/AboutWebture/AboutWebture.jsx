2860861a0138893fca110ec986a6a7c1703f0d58
Kunal Keshan <kunalkeshan12@gmail.com>
2022-06-03 17:55:30 +0530

    Basic Layout for about webture done

--- src/components/AboutWebture/AboutWebture.jsx
+++ src/components/AboutWebture/AboutWebture.jsx
@@ -5,13 +5,41 @@
 // Dependencies
 import React from 'react';
 
+import './AboutWebture.css'
+
 function AboutWebture() {
   return (
     <div className='container'>
+      {/* Browser Container */}
       <div className='container__browser'>
-        <div className='browser__tab'>Awesome Web Browser</div>
-        <div className='browser__navigation'></div>
-        <div className='browser__body'></div>
+
+        {/* Browser Tab */}
+        <div className='browser__tab'>
+          <span>Awesome Web Browser</span>
+          <i class="fa-solid fa-xmark"></i>
+          <div className='tab-design'></div>
+        </div>
+
+        {/* Browser Navigation */}
+        <div className='browser__navigation'>
+          <i className="fa-solid fa-arrow-left"></i>
+          <i className="fa-solid fa-arrow-right"></i>
+          <i className="fa-solid fa-rotate-right"></i>
+          <div className='browser__navigation-url'>
+            <i className="fa-solid fa-globe"></i>
+            <span>https://webture.ieeesrmist.in/about</span>
+          </div>
+          <i className="fa-solid fa-ellipsis-vertical"></i>
+        </div>
+
+        {/* Browser Body */}
+        <div className='browser__body'>
+          <div className='browser__body-content'>
+            <h2>About</h2>
+            <p>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>
+            <q>Lorem Ipsum Random quote for content</q>
+          </div>
+        </div>
       </div>
     </div>
   )
