2bd90556f6dbb2e7919d30b34901f1544533c11a
Snehil <snehilwebber@gmail.com>
2022-06-12 18:02:46 +0530

    changes

--- src/components/Teams/index.jsx
+++ src/components/Teams/index.jsx
@@ -1,15 +1,17 @@
 import { team } from "./team";
 import "./team-styles.scss";
+import { ReactComponent as Logo } from "./ieee-mask.svg";
 
 const Teams = () => {
   return (
     <main className="teamsWrapper">
       <h1>Our Speakers</h1>
       <div className="subtitle">Thank you for attending Webture!</div>
+      {/* <Logo className="mask-logo" /> */}
       <section>
         {team.map((item) => (
           <article>
-            <img src={item.img} />
+            <img src={item.img} alt={item.name} />
             <div className="content">
               <div className="name">{item.name}</div>
               <div className="ctas">
